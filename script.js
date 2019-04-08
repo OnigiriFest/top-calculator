469285efbc47cb01ab2db8b9b2f168f3758c943a
Facundo Panizza <facuelpato0@gmail.com>
2019-04-08 17:34:39 -0300

    Fixex bugs, better display and round numbers

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return Math.round((num1 + num2) * 100) / 100;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Math.round((num1 - num2) * 100) / 100;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Math.round((num1 * num2) * 100) / 100;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Math.round((num1 / num2) * 100) / 100;
 }
 
 function operate(string) {
@@ -99,38 +99,53 @@ function displayWrite(string) {
     let displayNow = document.querySelector('#now');
     let displayResult = document.querySelector('#result');
     let displayBefore = document.querySelector('#before');
-    let lastChar = displayResult.textContent[displayResult.textContent.length - 1];
+    let newResult = displayResult.textContent + string;
+    let lastChar = newResult[newResult.length - 1];
+    const regex = /[-x+÷]$/g;
 
-    if (displayResult.getAttribute('status') === 'result' && /[-x+÷]$/g.test(displayResult.textContent) === false) {
-        console.log('si entro');
-        displayBefore.textContent = displayNow.textContent + '=' + displayResult.textContent.slice(0, displayResult.textContent.length);
+    if(regex.test(string) && regex.test(displayNow.textContent)) {
+        return;
+    } 
+
+    console.log(newResult[newResult.length - 1]);
+    if(newResult[newResult.length - 1] === '.' && newResult[newResult.length -2] === '.') {
+        return;
+    }
+
+    if (displayResult.getAttribute('status') === 'result' && regex.test(newResult) === false) {
+        displayBefore.textContent = displayNow.textContent + '=' + newResult.slice(0, newResult.length - 1);
 
         displayNow.textContent = '';
-        displayResult.textContent = displayResult.textContent.slice(displayResult.textContent.length);
 
+        displayResult.textContent = string;
         displayResult.setAttribute('status', 'noresult');
-        
-        displayResult.textContent = displayResult.textContent + string;
-
         return
     }
 
     if(lastChar === '+' | lastChar === '-' | lastChar === 'x' | lastChar === '÷') {
-        if(displayResult.getAttribute('status') === 'result') {
-            console.log('si entro');
-            displayBefore.textContent = displayNow.textContent + '=' + displayResult.textContent.slice(0, displayResult.textContent.length - 1);
+        if(displayResult.textContent === '') {
+            displayResult.textContent = string;
+            return;
+        } else if(displayResult.textContent === '+' | displayResult.textContent === '-' | displayResult.textContent === '÷' | displayResult.textContent === 'x') {
+            return;
+        }
 
-            displayNow.textContent = displayResult.textContent;
+        if(displayResult.getAttribute('status') === 'result') {
+            displayBefore.textContent = displayNow.textContent + '=' + displayResult.textContent;
+
+            displayNow.textContent = newResult;
             displayResult.textContent = '';
 
             displayResult.setAttribute('status', 'noresult');
+
+            return;
         }
 
-        displayNow.textContent = displayNow.textContent + displayResult.textContent;
-        displayResult.textContent = '';
+        displayNow.textContent = displayNow.textContent + newResult;
+        newResult = '';
     } 
 
-    displayResult.textContent = displayResult.textContent + string;
+    displayResult.textContent = newResult;
 }
 
 function catcher(e) {
@@ -151,15 +166,19 @@ function catcher(e) {
         case 'history':
             return;
         case 'C':
-            if(displayNow.textContent === ''){
+            if(displayResult.getAttribute('status') === 'result') {
+                let actualResult = document.querySelector('#result');
+                let actualNow = document.querySelector('#now');
+
+                displayBefore.textContent = actualNow.textContent + '=' + actualResult.textContent;
                 displayResult.textContent = '';
+                displayNow.textContent = ''
+
                 displayResult.setAttribute('status', 'noresult');
-                return;
             }
 
-            displayBefore.textContent = operation + '=' + result;
-            displayNow.textContent = '';
             displayResult.textContent = '';
+            displayNow.textContent = ''
             return;
         case '=':
             displayNow.textContent = operation;
@@ -174,7 +193,11 @@ function catcher(e) {
             return;
     }
 
-    displayWrite(e.target.value);
+    if(string.length > 10) {
+        return;
+    } else {
+        displayWrite(e.target.value);
+    }
 }
 
 let buttons = Array.from(document.querySelectorAll('.btn'));
